3907fb2aef16d38d84060ac30dceb7d56f089236
Erika Gutierrez <erikagutierrez01@gmail.com>
2016-12-11 00:29:25 -0400

    Realtime messages Saved messages into database

--- src/services/index.js
+++ src/services/index.js
@@ -1,16 +1,18 @@
 'use strict';
+const message = require('./message');
 const authentication = require('./authentication');
 const user = require('./user');
 const Sequelize = require('sequelize');
 module.exports = function() {
   const app = this;
-  
+
   const sequelize = new Sequelize(app.get('postgres'), {
     dialect: 'postgres',
     logging: false
   });
   app.set('sequelize', sequelize);
-  
+
   app.configure(authentication);
   app.configure(user);
+  app.configure(message);
 };
